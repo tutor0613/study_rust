02242b17d35b59d9e54047fd5172f9b7ef525260
Youngpyeong Kim <tutor0613@github.com>
2026-01-30 18:31:10 +0900

    update: rustlings 16/94

--- 03_if/if1.rs
+++ 03_if/if1.rs
@@ -4,10 +4,19 @@ fn bigger(a: i32, b: i32) -> i32 {
     // Do not use:
     // - another function call
     // - additional variables
+    if a > b {
+        a
+    } else {
+        b
+    }
 }
 
 fn main() {
     // You can optionally experiment here.
+    let a: i32 = 1;
+    let b: i32 = 5;
+    
+    println!("a : {a}, b : {b}, bigger : {}", bigger(a, b));
 }
 
 // Don't mind this for now :)
