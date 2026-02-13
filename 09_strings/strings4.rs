bff8153d5b38388a15068ed00d1baf78ac63b56e
Youngpyeong Kim <tutor0613@github.com>
2026-02-13 09:29:02 +0900

    update: rustlings 40/94

--- 09_strings/strings4.rs
+++ 09_strings/strings4.rs
@@ -1,5 +1,23 @@
+trait Print {
+    fn print(&self);
+}
+
+impl Print for &str {
+    fn print(&self) {
+        string_slice(self);
+    }
+}
+
+impl Print for String {
+    fn print(&self) {
+        string(self.clone());
+    }
+}
+
 // Calls of this function should be replaced with calls of `string_slice` or `string`.
-fn placeholder() {}
+fn placeholder(input: impl Print) {
+    input.print();
+}
 
 fn string_slice(arg: &str) {
     println!("{arg}");
@@ -21,7 +39,8 @@ fn main() {
 
     placeholder("rust is fun!".to_owned());
 
-    placeholder("nice weather".into());
+    let msg: String = "nice weather".into();
+    placeholder(msg);
 
     placeholder(format!("Interpolation {}", "Station"));
 
