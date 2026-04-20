0b5bd897618e8d275bbde0a531e612c8c9f2310b
Youngpyeong Kim <kyp970115@gmail.com>
2026-04-20 23:47:30 +0900

    update: rustlings 53/94

--- 13_error_handling/errors3.rs
+++ 13_error_handling/errors3.rs
@@ -15,12 +15,18 @@ fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
 
 // TODO: Fix the compiler error by changing the signature and body of the
 // `main` function.
-fn main() {
+fn main() -> Result<(), ParseIntError> {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
     // Don't change this line.
     let cost = total_cost(pretend_user_input)?;
+    // It's same to below.
+    // let result = total_cost(pretend_user_input);
+    // let cost = match result {
+    //     Ok(val) => val,
+    //     Err(e) => return Err(e),
+    // };
 
     if cost > tokens {
         println!("You can't afford that many!");
@@ -28,4 +34,6 @@ fn main() {
         tokens -= cost;
         println!("You now have {tokens} tokens.");
     }
+
+    Ok(())
 }
