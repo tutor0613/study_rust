3f3aac2751b53ed30544d81107d3b56325e22a2d
Youngpyeong Kim <tutor0613@github.com>
2026-05-25 18:46:20 +0900

    update: rustlings 79/94

--- 18_iterators/iterators5.rs
+++ 18_iterators/iterators5.rs
@@ -28,6 +28,7 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // `map` is a hash map with `String` keys and `Progress` values.
     // map = { "variables1": Complete, "from_str": None, … }
+    map.values().filter(|val| **val == value).count()
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -48,6 +49,30 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // `collection` is a slice of hash maps.
     // collection = [{ "variables1": Complete, "from_str": None, … },
     //               { "variables2": Complete, … }, … ]
+
+    // My answer
+    collection.iter().map(|item| item.values().filter(|val| **val == value).count()).sum()
+
+    // For visibility
+    // collection
+    //     .iter()
+    //     .map(|item| count_iterator(item, value))
+    //     .sum()
+
+    // Using flat_map
+    // collection
+    //     .iter()
+    //     .flat_map(HashMap::values)
+    //     .filter(|val| **val == value)
+    //     .count()
+
+    // To use flatten()
+    // collection
+    //     .iter()
+    //     .map(HashMap::values)
+    //     .flatten()
+    //     .filter(|val| **val == value)
+    //     .count()
 }
 
 fn main() {
